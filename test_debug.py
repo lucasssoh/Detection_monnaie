6cb9000cf2550c55a6c70d061c76e2a34b22272a
shisato5 <cindychhieng@gmail.com>
2026-05-24 12:01:41 +0200

    inversion binarisation en cas de mauvais resultat + ajout test

---
+++ test_debug.py
@@ -0,0 +1,3 @@
+from preprocessing import debug_image
+debug_image("base_images/1000016159.jpg", output="debug_8pieces.png")      # vrai = 8
+debug_image("base_images\WhatsApp Image 2026-02-08 at 16.01.09 (2).jpeg", output="debug_1piece.png")      # vrai = 1
